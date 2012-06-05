61ddb75ee6ac51d9957b4a23228c3852cb1cc717
Yann Schwartz <abolibibelot@gmail.com>
2012-06-05 22:56:01 +0200

    Ugly implem for 156

--- src/solutions.clj
+++ src/solutions.clj
@@ -1,8 +1,9 @@
 (ns solutions)
 
 ;156 Map Defaults
-(defn (fn156[]
-  ))
+(defn fn156[d k]
+  (apply hash-map (interleave k (repeat 10 d)) 
+    ))
 
 (= (fn156 0 [:a :b :c]) {:a 0 :b 0 :c 0})	
 (= (fn156 "x" [1 2 3]) {1 "x" 2 "x" 3 "x"})	
