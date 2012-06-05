4449e95e0400b37476ebdec0bfe02706e649ec81
Yann Schwartz <abolibibelot@gmail.com>
2012-06-05 22:51:21 +0200

    156 added

---
+++ src/solutions.clj
@@ -0,0 +1,9 @@
+(ns solutions)
+
+;156 Map Defaults
+(defn (fn156[]
+  ))
+
+(= (fn156 0 [:a :b :c]) {:a 0 :b 0 :c 0})	
+(= (fn156 "x" [1 2 3]) {1 "x" 2 "x" 3 "x"})	
+(= (fn156 [:a :b] [:foo :bar]) {:foo [:a :b] :bar [:a :b]})
