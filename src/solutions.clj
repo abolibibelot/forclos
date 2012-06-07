9ef934c493a68956f1258b6ffc62f9fec55074a5
Yann Schwartz <abolibibelot@gmail.com>
2012-06-07 08:08:47 +0200

    22 done

--- src/solutions.clj
+++ src/solutions.clj
@@ -1,4 +1,14 @@
 (ns solutions)
+;22 Count
+(defn fn22[coll]
+  (reduce (fn [n _](inc n)) 0 [1 2 3]))
+
+(= (fn22 '(1 2 3 3 1)) 5)	
+(= (fn22 "Hello World") 11)	
+(= (fn22 [[1 2] [3 4] [5 6]]) 3)	
+(= (fn22 '(13)) 1)	
+(= (fn22 '(:a :b :c)) 3)
+
 
 ;156 Map Defaults
 (defn fn156[d k]
@@ -22,4 +32,4 @@
 (= :gt (fn166 < 5 1))	
 (= :eq (fn166 (fn [x y] (< (count x) (count y))) "pear" "plum"))	
 (= :lt (fn166 (fn [x y] (< (mod x 5) (mod y 5))) 21 3))	
-(= :gt (fn166 > 0 2)))
+(= :gt (fn166 > 0 2))))
